1c4acf4a9bf9616487b3926a7c6e4be429452a44
publdaze <publdaze@naver.com>
2023-06-27 22:05:51 +0900

    feat: 회원가입 상단바 구현
    - UI 구성
    - 뒤로가기 버튼 클릭시 시작화면으로 가도록 구현
    
    #3

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { View } from "react-native";
+import { Appbar } from "react-native-paper";
 
-function SignUp() {
-  return <View></View>;
+function SignUp({ navigation }) {
+  return (
+    <View>
+      <Appbar mode="center-aligned" safeAreaInsets={{ top: 20 }}>
+        <Appbar.BackAction onPress={() => navigation.navigate("Start")} />
+        <Appbar.Content title="회원가입" />
+      </Appbar>
+    </View>
+  );
 }
 
 export default SignUp;
