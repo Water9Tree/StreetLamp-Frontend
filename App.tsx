bb06ed99f07ca7bc41af6217c0a1275c2c9c8a20
publdaze <publdaze@naver.com>
2023-06-27 20:26:54 +0900

    chore: react-native-paper 설치
    #1

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>가로등 모니터링 서비스</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>가로등 모니터링 서비스</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
