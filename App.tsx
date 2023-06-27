7170da88208a3dbe70dae547f0b125d6f7bdd685
publdaze <publdaze@naver.com>
2023-06-27 21:13:21 +0900

    feat: 메인 페이지 UI 구성
    #2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Button, PaperProvider, Text } from "react-native-paper";
+import Icon from "react-native-paper/src/components/Icon";
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>가로등 모니터링 서비스</Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginBottom: 30,
+          }}
+        >
+          <Icon size={70} source="floor-lamp-torchiere-outline"></Icon>
+          <Text variant="displaySmall">PNU</Text>
+        </View>
+        <Text variant="headlineMedium">가로등 모니터링 서비스</Text>
+        <View
+          style={{
+            position: "absolute",
+            bottom: 130,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            style={{ marginRight: 10 }}
+            mode="outlined"
+            onPress={() => console.log("Pressed")}
+          >
+            회원가입
+          </Button>
+          <Button mode="contained-tonal" onPress={() => console.log("Pressed")}>
+            로그인
+          </Button>
+        </View>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -15,6 +48,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    position: "relative",
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
