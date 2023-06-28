5b784e6182c57fd016efd92c1476e25dc0c54dde
publdaze <publdaze@naver.com>
2023-06-29 00:32:34 +0900

    design: 회원가입 화면 스타일 추가
    #3

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { View } from "react-native";
 import { Appbar } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
 import HeaderBar from "../components/HeaderBar";
 
 function SignUp({ navigation }) {
   return (
-    <View>
+    <View style={styles.container}>
       <HeaderBar
         navigation={navigation}
         backScreen={"Start"}
@@ -14,5 +15,12 @@ function SignUp({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
 
 export default SignUp;
