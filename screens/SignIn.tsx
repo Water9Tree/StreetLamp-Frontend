439723bbbae130075e2ee47b11e80fe1da0d1964
publdaze <publdaze@naver.com>
2023-06-29 02:05:08 +0900

    feat: 회원가입 페이지와 동일하게 구성
    #4

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,8 +1,83 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, HelperText, TextInput } from "react-native-paper";
+import HeaderBar from "../components/HeaderBar";
 
-function SignIn() {
-  return <View></View>;
+function SignIn({ navigation }) {
+  const [id, setId] = useState({ value: "", error: "" });
+  const [pw, setPw] = useState({ value: "", error: "" });
+  const [pwSecureTextEntry, setPwSecureTextEntry] = useState(true);
+  const pwRef = useRef();
+
+  const handleSubmitButtonPress = () => {
+    const isInValid = false; // TODO 유효성 검사
+
+    if (isInValid) return;
+  };
+
+  return (
+    <View style={styles.container}>
+      <HeaderBar
+        navigation={navigation}
+        backScreen={"Start"}
+        title={"로그인"}
+      />
+      <View style={{ padding: 25, marginTop: 40 }}>
+        <View>
+          <TextInput
+            label="아이디"
+            returnKeyType="next"
+            value={id.value}
+            onChangeText={(text) => setId({ value: text, error: "" })}
+            onSubmitEditing={() => {
+              pwRef.current.focus();
+            }}
+            error={Boolean(id.error)}
+          />
+          <HelperText type="error" visible={Boolean(id.error)}>
+            {id.error}
+          </HelperText>
+        </View>
+        <View>
+          <TextInput
+            ref={pwRef}
+            label="비밀번호"
+            returnKeyType="done"
+            value={pw.value}
+            onChangeText={(text) => setPw({ value: text, error: "" })}
+            onSubmitEditing={handleSubmitButtonPress}
+            error={Boolean(pw.error)}
+            secureTextEntry={pwSecureTextEntry}
+            right={
+              <TextInput.Icon
+                icon={pwSecureTextEntry ? "eye-off" : "eye"}
+                onPress={() => {
+                  setPwSecureTextEntry((prev) => !prev);
+                }}
+              />
+            }
+          />
+          <HelperText type="error" visible={Boolean(pw.error)}>
+            {pw.error}
+          </HelperText>
+        </View>
+        <Button
+          mode="contained"
+          onPress={handleSubmitButtonPress}
+          style={styles.button}
+        >
+          확인
+        </Button>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
 
 export default SignIn;
