355ad08f73a7e509754ba737a22ad245d229f9c7
publdaze <publdaze@naver.com>
2023-06-29 08:40:28 +0900

    feat: 메인 화면 이미지 출력
    #5

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, ImageBackground, View } from "react-native";
+const mapImage = require("../assets/부산대맵.png");
 
-function Main(props) {
-  return <View></View>;
+function Main() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={mapImage}
+        resizeMode="contain"
+        style={{
+          flex: 1,
+          justifyContent: "center",
+        }}
+      ></ImageBackground>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
 
 export default Main;
