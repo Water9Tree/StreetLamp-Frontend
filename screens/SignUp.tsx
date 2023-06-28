cbd7533b492de71483d30d6cf5ad00b0f093a8f7
publdaze <publdaze@naver.com>
2023-06-29 00:33:42 +0900

    feat: 아이디, 비밀번호 입력 폼 구성
    #3

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { View } from "react-native";
-import { Appbar } from "react-native-paper";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { HelperText, TextInput } from "react-native-paper";
 import HeaderBar from "../components/HeaderBar";
 
 function SignUp({ navigation }) {
+  const [id, setId] = useState({ value: "", error: "" });
+  const [pw, setPw] = useState({ value: "", error: "" });
+
   return (
     <View style={styles.container}>
       <HeaderBar
@@ -12,6 +14,34 @@ function SignUp({ navigation }) {
         backScreen={"Start"}
         title={"회원가입"}
       />
+      <View style={{ padding: 25, marginTop: 40 }}>
+        <View>
+          <TextInput
+            label="아이디"
+            returnKeyType="next"
+            value={id.value}
+            onChangeText={(text) => setId({ value: text, error: "" })}
+            error={Boolean(id.error)}
+          />
+          <HelperText type="error" visible={Boolean(id.error)}>
+            {id.error}
+          </HelperText>
+        </View>
+        <View>
+          <TextInput
+            label="비밀번호"
+            returnKeyType="done"
+            value={pw.value}
+            onChangeText={(text) => setPw({ value: text, error: "" })}
+            error={Boolean(pw.error)}
+            secureTextEntry
+            right={<TextInput.Icon icon="eye" />}
+          />
+          <HelperText type="error" visible={Boolean(pw.error)}>
+            {pw.error}
+          </HelperText>
+        </View>
+      </View>
     </View>
   );
 }
