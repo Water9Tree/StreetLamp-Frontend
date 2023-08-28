fcc635c014241de1efa6dd97badcf386e4146640
publdaze <publdaze@naver.com>
2023-08-28 15:08:40 +0900

    feat: 이전 가로등 위치 받아오기

--- components/LampEditModal.tsx
+++ components/LampEditModal.tsx
@@ -1,15 +1,65 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { Button, Dialog, Portal, TextInput } from "react-native-paper";
+import React, { useEffect, useRef, useState } from "react";
+import { ImageBackground, PanResponder, View } from "react-native";
+import {
+  Button,
+  Dialog,
+  IconButton,
+  Portal,
+  TextInput,
+} from "react-native-paper";
+const mapImage = require("../assets/부산대맵.png");
 
 const LampEditModal = ({ visible, setVisible, lampData }: any) => {
   const [lampName, setLampName] = useState(lampData?.lampName);
   const [adjoiningPlace, setAdjoiningPlace] = useState(
     lampData?.adjoiningPlace
   );
+  const [isNext, setIsNext] = useState(false);
+
+  const imageRef = useRef<View>(null);
+  const [clickCoordinates, setClickCoordinates] = useState({ x: 0, y: 0 });
+  const [marker, setMarker] = useState<React.ReactNode | null>(null);
+
+  const handlePress = (event: any) => {
+    event.persist(); // Preserve the event
+    if (imageRef.current) {
+      imageRef.current.measure((fx, fy, width, height, px, py) => {
+        const x = event.nativeEvent.pageX - px;
+        const y = event.nativeEvent.pageY - py;
+        setClickCoordinates({ x: x - 50, y: y - 50 });
+        console.log(x, y);
+        setMarker(
+          <View
+            style={{
+              position: "absolute",
+              top: y - 50,
+              left: x - 50,
+            }}
+          >
+            <IconButton
+              icon="map-marker"
+              size={50}
+              iconColor={"gray"}
+              disabled
+            />
+          </View>
+        );
+      });
+    }
+  };
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        handlePress(evt);
+      },
+    })
+  ).current;
 
   const hideDialog = () => {
-    axios
+    /* axios
       .patch(`/lamps/${lampData.lampId}`, {
         lampName,
         adjoiningPlace,
@@ -20,11 +70,29 @@ const LampEditModal = ({ visible, setVisible, lampData }: any) => {
       })
       .catch(function (error) {
         console.log(error);
-      });
+      }); */
   };
   const hideDialog2 = () => {
     setVisible(false);
+    setIsNext(false);
   };
+
+  useEffect(() => {
+    if (!lampData) return;
+    setMarker(
+      <View
+        style={{
+          position: "absolute",
+          top: lampData?.location.y,
+          left: lampData?.location.x,
+        }}
+      >
+        <IconButton icon="map-marker" size={50} iconColor={"dimgray"} />
+      </View>
+    );
+    setLampName(lampData?.lampName);
+  }, [lampData]);
+
   return (
     <Portal>
       <Dialog
@@ -33,29 +101,63 @@ const LampEditModal = ({ visible, setVisible, lampData }: any) => {
         onDismiss={hideDialog}
       >
         <Dialog.Title style={{ fontSize: 16 }}>가로등 수정</Dialog.Title>
-        <Dialog.Content>
-          <TextInput
-            style={{ marginBottom: 30 }}
-            label="가로등명"
-            value={lampName ?? lampData?.lampName}
-            onChangeText={(text) => setLampName(text)}
-          />
-          <TextInput
+        {!isNext ? (
+          <>
+            <Dialog.Content>
+              <TextInput
+                style={{ marginBottom: 30 }}
+                label="가로등명"
+                value={lampName ?? lampData?.lampName}
+                onChangeText={(text) => setLampName(text)}
+              />
+              {/* <TextInput
             label="인접한 장소"
             value={adjoiningPlace ?? lampData?.adjoiningPlace}
             onChangeText={(text) => setAdjoiningPlace(text)}
-          />
-        </Dialog.Content>
-        <Dialog.Actions>
-          <Button onPress={hideDialog2}>취소</Button>
-          <Button
-            onPress={hideDialog}
-            mode="contained"
-            style={{ paddingHorizontal: 6 }}
-          >
-            수정
-          </Button>
-        </Dialog.Actions>
+          /> */}
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={hideDialog2}>취소</Button>
+              <Button
+                onPress={() => {
+                  setIsNext(true);
+                }}
+                mode="contained"
+                style={{ paddingHorizontal: 6 }}
+              >
+                다음
+              </Button>
+            </Dialog.Actions>
+          </>
+        ) : (
+          <>
+            <Dialog.Content>
+              <View
+                {...panResponder.panHandlers}
+                ref={imageRef}
+                collapsable={false}
+              >
+                <ImageBackground
+                  source={mapImage}
+                  resizeMode="cover"
+                  style={{ position: "relative", height: 400 }}
+                >
+                  {marker}
+                </ImageBackground>
+              </View>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={hideDialog2}>취소</Button>
+              <Button
+                onPress={hideDialog}
+                mode="contained"
+                style={{ paddingHorizontal: 6 }}
+              >
+                수정
+              </Button>
+            </Dialog.Actions>
+          </>
+        )}
       </Dialog>
     </Portal>
   );
